87dea00831de784e5ede514810de2ddb8795f02c
juan2025-del <juan1113725v@gmail.com>
2025-11-18 00:28:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
 function consultarAlaApi() {
 
-let urlConsulta = "https://opensheet.elk.sh/13C3PithGPmHtzHvZcIL_w4-n0CgFkZ_zgRyhUoc-g40/Hoja%201";
-
+  let urlConsulta = "https://opensheet.elk.sh/13C3PithGPmHtzHvZcIL_w4-n0CgFkZ_zgRyhUoc-g40/Hoja1";
 
   let mensajeResultado = document.getElementById("mensajeResultado");
 
@@ -11,11 +10,14 @@ let urlConsulta = "https://opensheet.elk.sh/13C3PithGPmHtzHvZcIL_w4-n0CgFkZ_zgRy
   fetch(urlConsulta)
     .then(response => response.json())
     .then(datos => {
-      console.log(datos); 
-      mensajeResultado.innerHTML = "En la consola encontrarás el resultado obtenido";
+      console.log("Datos obtenidos:", datos);
+
+      mensajeResultado.innerHTML = "Datos cargados correctamente ✔";
+
+      // Si quieres ver los datos en pantalla luego podemos agregarlo.
     })
     .catch(error => {
       mensajeResultado.innerHTML = "Error al cargar los datos 😢";
-      console.error(error);
+      console.error("Error:", error);
     });
 }
